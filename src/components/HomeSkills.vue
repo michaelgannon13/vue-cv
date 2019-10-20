61e15d369515ac4c996763de670e2edde9dd06c6
Michael <m.j.gannon@accenture.com>
2019-10-20 20:01:50 +0200

    added skills section

---
+++ src/components/HomeSkills.vue
@@ -0,0 +1,200 @@
+<template>
+  <v-container grid-list-lg>
+    <v-layout row>
+      <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">What I can help with</v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/hjCA3ecCXAQ" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Prototyping</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Prototyping</h3>
+              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/6S27S6pZ6o0" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">User Interface</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">User Interface</h3>
+              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/1SPu0KT-Ejg" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Design Thinking</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Design Thinking</h3>
+              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/hjCA3ecCXAQ" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Optimization</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Optimization</h3>
+              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/6S27S6pZ6o0" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Search Engine Optimization</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Search Engine Optimization</h3>
+              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/1SPu0KT-Ejg" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Responsive Web Design</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Responsive Web Design</h3>
+              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/hjCA3ecCXAQ" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Mobile Development</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Mobile Development</h3>
+              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/6S27S6pZ6o0" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Wordpress</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Wordpress</h3>
+              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/1SPu0KT-Ejg" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Identity / Services</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Identity / Services</h3>
+              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HomePlans"
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
     <span>
         <home-hero></home-hero>
         <home-details></home-details>
+        <home-skills></home-skills>
     </span>
 </template>
 
 <script>
 import HomeHero from '@/components/HomeHero';
 import HomeDetails from '@/components/HomeDetails';
+import HomeSkills from '@/components/HomeSkills';
 
 export default {
     name: 'home',
     components: {
         HomeHero, 
-        HomeDetails
+        HomeDetails,
+        HomeSkills  
     }
 };
 </script>
