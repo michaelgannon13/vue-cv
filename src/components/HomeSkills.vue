f5b04d518042c4eec93945902d046edc84a2d8d3
Michael <m.j.gannon@accenture.com>
2019-10-21 13:54:30 +0200

    added skills copy

--- src/components/HomeSkills.vue
+++ src/components/HomeSkills.vue
@@ -19,7 +19,8 @@
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">Prototyping</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+              <div>Wireframe to reality.
+Proficient in Adobe Photoshop and XD. I've experience creating high fidelity prototypes rapidly and as well as diligently and informed by user feedback. Balsamique also. Pen and paper prototypes is the old reliable </div>
             </div>
           </v-card-title>
         </v-card>
@@ -39,7 +40,29 @@
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">User Interface</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+              <div>My specialty for 5+ years. Every aspect relating to the DOM. From creating static Photoshop files to implementing as robust, scalable solutions with an emphasis on user experience </div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/hjCA3ecCXAQ" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Javascript</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Javascript</h3>
+              <div>Angular js and Angular for 5 years. Experience with Vue js, stencil js, react, backbone js, all versions of Bootstrap, Google material, bulma, jQuery and plenty of vanilla JS</div>
             </div>
           </v-card-title>
         </v-card>
@@ -59,7 +82,7 @@
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">Design Thinking</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+              <div>Founder of the Accenture Design thinking guild. Monthly presentations and spreading the good work. Bringing Dt into all projects. Presenter at the design thinking academy workshops </div>
             </div>
           </v-card-title>
         </v-card>
@@ -80,7 +103,7 @@
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">Optimization</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+              <div>Always been fascinated with improving performance where possible in the web, especially the quick-win changes that have big impact and small overhead.</div>
             </div>
           </v-card-title>
         </v-card>
@@ -100,7 +123,7 @@
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">Search Engine Optimization</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+              <div>Founder of simplecss.eu. a pet project that gained popularity, yielded ad revenue and sold for profit. Storing domain authority highly ranked in Google search results.</div>
             </div>
           </v-card-title>
         </v-card>
@@ -120,7 +143,7 @@
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">Responsive Web Design</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+              <div>Using bootstrap since it was in its infancy. I lean toward as mobile first approach and an efficient use of space. Experience with bulma, foundation aswell as flexbox, CSS grid and angular flex layout</div>
             </div>
           </v-card-title>
         </v-card>
@@ -141,7 +164,7 @@
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">Mobile Development</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+              <div>Extremely enthusiastic about ionic and integrating with other js frameworks angular, react etc. Ionic, angular and stencil. Built a number of applications using ionic. As a front end engineer I lean towards this approach but have also built apps using native Java and XML for Android, the good old fashioned way!</div>
             </div>
           </v-card-title>
         </v-card>
@@ -161,7 +184,8 @@
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">Wordpress</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+              <div>Extensive experience with WordPress websites using themes, customizing themes, from scratch, for blogs, for sites. This my staple CMS
+</div>
             </div>
           </v-card-title>
         </v-card>
@@ -173,15 +197,75 @@
             <v-container fill-height fluid>
               <v-layout fill-height>
                 <v-flex xs12 align-end flexbox>
-                  <span class="headline white--text">Identity / Services</span>
+                  <span class="headline white--text">Identity</span>
                 </v-flex>
               </v-layout>
             </v-container>
           </v-img>
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">Identity / Services</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+              <h3 class="headline mb-0">Identity</h3>
+              <div>Logo creation, custom websites, blogs, graphic design, branding guidelines, colour palettes, consistency. Created identities for small projects to large scale organisations.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/1SPu0KT-Ejg" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Workflow</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Workflow</h3>
+              <div>Agile - Scrum, Git flow, BEM</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/1SPu0KT-Ejg" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Services</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Services</h3>
+              <div>Experience writing custom services leveraging 3rd party APIs, consuming response and parsing into human readable format only he UI. </div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm12 md4>
+        <v-card>
+          <v-img src="http://source.unsplash.com/1SPu0KT-Ejg" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">Accessibility</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Accessibility</h3>
+              <div>Pa11y, wave, screen reader, Google lighthouse audits </div>
             </div>
           </v-card-title>
         </v-card>
